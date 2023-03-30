06b655eb95720cbaf0f41db09b5ec284b1324704
keathkeather <67945664+keathkeather@users.noreply.github.com>
2023-03-30 21:09:49 +0800

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import face_recognition
 import os
-from datetime import datetime
+
 
 path = 'image attendace'
 images = []
@@ -36,20 +36,21 @@ cap = cv2.VideoCapture(0)
 while True:
     success, img = cap.read()
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-    imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
     faceCurFrame = face_recognition.face_locations(imgS)
     encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
 
     for encodeFace, FaceLoc in zip(encodeCurFrame, faceCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        print(faceDis)
+        #print(faceDis)
         matchIndex = np.argmin(faceDis)
 
         if matches[matchIndex]:
             name = classname[matchIndex].upper()
             print(name)
             y1,x2,y2,x1 = FaceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
 
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
             cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
@@ -57,4 +58,5 @@ while True:
 
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)
+    
 
